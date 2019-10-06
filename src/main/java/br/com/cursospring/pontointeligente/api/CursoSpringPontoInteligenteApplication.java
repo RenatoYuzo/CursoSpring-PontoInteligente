941c1eefb84b43157c830053c7edf73bf6eeaf32
RenatoYuzo <renatoyuzo93@gmail.com>
2019-10-05 20:36:29 -0400

    First Commit

---
+++ src/main/java/br/com/cursospring/pontointeligente/api/CursoSpringPontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursospring.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringPontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringPontoInteligenteApplication.class, args);
+	}
+
+}
